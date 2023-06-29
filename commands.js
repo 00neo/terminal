b1d1a17967306844fad2c58035111c52c7481e48
StuckInPlace <137904193+StuckInPlace@users.noreply.github.com>
2023-06-29 21:44:47 +0200

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,95 @@
+
+
+var password = "Hhcfj4pd";
+var github = "https://github.com/advancedfx/advancedfx/releases/tag/v2.146.0";
+var email = 'mailto:billyollech@gmail.com';
+var discord = "https://discord.gg/proxyboost";
+var cash = "https://www.paypal.com/donate/?hosted_button_id=Q5M5RQS26YV7C";
+var bitcoin = "bitcoin:1K7S67iJVtGBGc17UiCp9xwZ3xnPfPxvrE"
+var ethereum = "ethereum:0xAc18d9A4Bc75eA3E7d255f11f27D82872ea6FCfc@1"
+
+whois = [
+  "<br>",
+  "Hi I am Sip , I am a webdeveloper",
+  
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Could not find a definition.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'discord         <a href="' + discord + '" target="_blank">discord/stuckinplace' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/stuckinplace' + "</a>",
+  "<br>"
+];
+
+
+
+donate = [
+    "<br>",
+    'Cash         <a href="' + cash + '" target="_blank">sendcash/stuckinplace' + "</a>",
+    'Bitcoin         <a href="' + bitcoin + '" target="_blank">bitcoin/wallet' + "</a>",
+    'Ethereum         <a href="' + ethereum + '" target="_blank">ethereum/wallet' + "</a>",
+    "<br>"
+  ];
+
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Most Projects are on Github, name: StuckInPlace",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          StuckInPlace?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">donatef</span>        All future you will get while donating',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">donate</span>         Donate cash',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           help system',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear all , type banner for starting',
+  '<span class="command">banner</span>         restart System',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Stuck In Place (SIP) W</span>',
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\          _____ ________   ",
+  "      '`  ` |-|   `` '                 / ___//  _/ __ \ ",
+  "            |-|                        \__ \ / // /_/ / ",
+  "            |-|O                      ___/ // // ____/  ",
+  "            |-(\\,__                  /____/___/_/       ",
+  "         ...|-|\\--,\\_....                                                ",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome my friend !</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+donatefeaturestxt = [
+
+  "Custom Designs 8&#8364;",
+  "Full made Discord Server 10&#8364;",
+  "Custom Bots mit allen Funktionen 165&#8364;",
+  "Web, Disc , Code Templates 6&#8364;",
+  "Fully Made Websites 25-505&#8364;"
+]
