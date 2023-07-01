ac4102a3128d3874ad6d869144f3254d40e29084
NEO <137904193+00neo@users.noreply.github.com>
2023-07-01 16:10:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("stuckinplace:~$ " + command.innerHTML, "no-animation", 0);
+      addLine("00neo00:~$ " + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
