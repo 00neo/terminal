dd4b845dcd98e7346e992c42bce8bb16a9e648d5
NEO <137904193+00neo@users.noreply.github.com>
2023-07-01 16:09:48 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -3,7 +3,7 @@
 var password = "Hhcfj4pd";
 var github = "https://github.com/advancedfx/advancedfx/releases/tag/v2.146.0";
 var email = 'mailto:billyollech@gmail.com';
-var discord = "https://discord.gg/00neo00";
+var discord = "https://discord.gg/proxyboost";
 var cash = "https://www.paypal.com/donate/?hosted_button_id=5HDVT6LP6K6S2";
 var bitcoin = "bitcoin:1K7S67iJVtGBGc17UiCp9xwZ3xnPfPxvrE"
 var ethereum = "ethereum:0xAc18d9A4Bc75eA3E7d255f11f27D82872ea6FCfc@1"
@@ -70,18 +70,39 @@ help = [
 
 banner = [
   '<span class="index">Connection:..neo-terminal...successfull </span>',
-  "         ___   ____",
-  "       /' --;^/ ,-_\\     \\ | /       ",
-  "      / / --o\\ o-\\ \\\\   --(_)--   ",
-  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\           ",
-  "      '`  ` |-|   `` '                ",
-  "            |-|                       ",
-  "            |-|O                        ",
-  "            |-(\\,__                 ",
-  "         ...|-|\\--,\\_....                                                ",
-  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
-  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
-  '<span class="color2">Welcome my friend !</span>',
+  '<span class="color2">Welcome to the Terminal !</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −      √       π       ∈       ∉       ∋       ∞       ≈       ≠       ≤       ≥       ≅      ×       ÷       ·       •       ∠       °       ‰       ‾</span>',
+  '<span class="color2">±       −     σ      τ       υ      φ        χ       ψ       ω        Α      Β       Γ      Δ       Ε      Ζ      Η       Θ    Κ     Λ     Ν        ‾</span>',
+  '<span class="color2">±       −     σ      τ       υ      φ        χ       ψ       ω        Α      Β       Γ      Δ       Ε      Ζ      Η       Θ    Κ     Λ     Ν        ‾</span>',
+  '<span class="color2">±       −     σ      τ       υ      φ        χ       ψ       ω        Α      Β       Γ      Δ       Ε      Ζ      Η       Θ    Κ     Λ     Ν        ‾</span>',
+  '<span class="color2">±       −     σ      τ       υ      φ        χ       ψ       ω        Α      Β       Γ      Δ       Ε      Ζ      Η       Θ    Κ     Λ     Ν        ‾</span>',
+  '<span class="color2">±       −     σ      τ       υ      φ        χ       ψ       ω        Α      Β       Γ      Δ       Ε      Ζ      Η       Θ    Κ     Λ     Ν        ‾</span>',
+  '<span class="color2">±       −     0001000101010000001010100101010100101010101010101001010101010101010010101111111110000000000000100101010100101010101001001001000100010101001     ‾</span>',
+  '<span class="color2">±       −     0001000101010000001010100101010100101010101010101001010101010101010010101111111110000000000000100101010100101010101001001001000100010101001     ‾</span>',
+  '<span class="color2">±       −     0001000101010000001010100101010100101010101010101001010101010101010010101111111110000000000000100101010100101010101001001001000100010101001     ‾</span>',
+  '<span class="color2">±       −     0001000101010000001010100101010100101010101010101001010101010101010010101111111110000000000000100101010100101010101001001001000100010101001     ‾</span>',
+  '<span class="color2">±       −     0001000101010000001010100101010100101010101010101001010101010101010010101111111110000000000000100101010100101010101001001001000100010101001     ‾</span>',
+  '<span class="color2">±       −     0001000101010000001010100101010100101011101001010100101010100101010101010101000101010101000000100101010100101010101001001001000100010101001     ‾</span>',
+
+
+  
   "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
 ];
 
