055dc70ebaf37384a1c104d5dde7497ca3837d74
luisollech <137904193+luisollech@users.noreply.github.com>
2023-08-25 21:13:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("00neo00:~$ " + command.innerHTML, "no-animation", 0);
+      addLine("00luis:~$ " + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
